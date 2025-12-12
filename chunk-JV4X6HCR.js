56e17d83328bfc1a810a48fd8c1534eda9b6b840
ana.espejo <ana.espejo@totvs.com.br>
2025-12-12 14:35:46 -0300

    Auto-generated commit

--- chunk-JV4X6HCR.js
+++ chunk-JV4X6HCR.js
@@ -1,4 +1,4 @@
-import{a as D}from"./chunk-NPAVVAE3.js";import{a as C,b as S}from"./chunk-QWEEXONV.js";import"./chunk-2EAKA7BD.js";import{a as p}from"./chunk-UZANZAJW.js";import{Ac as O,Da as E,Ea as u,Na as l,Nc as g,Rc as h,V as s,Wc as f,Ya as o,Yf as x,Za as t,_a as d,cd as v,lg as b,sa as m,xa as M,xb as e,xd as c}from"./chunk-4CT7PI5G.js";var A=(()=>{class i{constructor(a){this.sharedService=a,this.commandDoc1=`export const <NOME-MODULO>Submenu: Array<PoMenuItem> = [
+import{a as D}from"./chunk-KNPNQ7SP.js";import{a as C,b as S}from"./chunk-QWEEXONV.js";import"./chunk-T7NQXMKK.js";import{a as p}from"./chunk-UZANZAJW.js";import{Ac as O,Da as E,Ea as u,Na as l,Nc as g,Rc as h,V as s,Wc as f,Ya as o,Yf as x,Za as t,_a as d,cd as v,lg as b,sa as m,xa as M,xb as e,xd as c}from"./chunk-4CT7PI5G.js";var A=(()=>{class i{constructor(a){this.sharedService=a,this.commandDoc1=`export const <NOME-MODULO>Submenu: Array<PoMenuItem> = [
   {
     label: '<NOME-APLICA\xC7\xC3O>',
     subItems: [
